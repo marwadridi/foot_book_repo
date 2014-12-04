d9d257a071c7445f5d8ad078dad6640893f0c5d1
maroua <maroua@maroua-PC>
2014-12-04 15:24:11 +0100

    initial commit
    Signed-off-by: maroua <maroua@maroua-PC>

---
+++ tn.edu.esprit.gl.foot_bookEJB/ejbModule/tn/edu/esprit/gl/foot_bookEJB/model/User.java
@@ -0,0 +1,67 @@
+package tn.edu.esprit.gl.foot_bookEJB.model;
+
+import java.io.Serializable;
+import java.lang.String;
+import java.util.List;
+
+import javax.persistence.*;
+
+/**
+ * Entity implementation class for Entity: User
+ *
+ */
+@Entity
+
+public class User implements Serializable {
+
+	
+	private int id;
+	private String name;
+	private String login;
+	private String password;
+	private static final long serialVersionUID = 1L;
+	
+	private List<Booking> bookings;
+	
+
+	public User() {
+		super();
+	}   
+	@Id    
+	public int getId() {
+		return this.id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}   
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}   
+	public String getLogin() {
+		return this.login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}   
+	public String getPassword() {
+		return this.password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	@OneToMany(mappedBy="user")
+	public List<Booking> getBookings() {
+		return bookings;
+	}
+	public void setBookings(List<Booking> bookings) {
+		this.bookings = bookings;
+	}
+   
+}
---
+++ tn.edu.esprit.gl.foot_bookWeb/src/tn/edu/esprit/gl/foot_bookWeb/ctr/StadiumCtr.java
@@ -0,0 +1,71 @@
+package tn.edu.esprit.gl.foot_bookWeb.ctr;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import javax.ejb.EJB;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.SessionScoped;
+import javax.faces.model.DataModel;
+import javax.faces.model.ListDataModel;
+
+import tn.edu.esprit.gl.foot_bookEJB.model.Stadium;
+import tn.edu.esprit.gl.foot_bookEJB.model.User;
+import tn.edu.esprit.gl.foot_bookEJB.services.interfaces.BookingServicesLocal;
+import tn.edu.esprit.gl.foot_bookEJB.services.interfaces.StadiumServicesLocal;
+
+@ManagedBean(name = "stadiumManager")
+@SessionScoped
+public class StadiumCtr {
+	private List<Stadium> stadiums = new ArrayList<Stadium>();
+	private Stadium stadiumSelected = new Stadium();
+	private DataModel<Stadium> datamodel = new ListDataModel<Stadium>();
+	private User userConnected = new User();
+
+	@ManagedProperty(value = "#{userManager}")
+	private UserCtr userCtr;
+
+	@EJB
+	private StadiumServicesLocal stadiumServicesLocal;
+	@EJB
+	private BookingServicesLocal bookingServicesLocal;
+
+	public String showSelectedStadium() {
+		stadiumSelected = datamodel.getRowData();
+		userConnected = userCtr.getUser();
+		System.out.println(stadiumSelected.getLibele());
+		System.out.println(userConnected.getName());
+		bookingServicesLocal.bookStadium(userConnected, stadiumSelected,
+				new Date());
+		return "";
+	}
+
+	public List<Stadium> getStadiums() {
+		stadiums = stadiumServicesLocal.findAllStadiums();
+		return stadiums;
+	}
+
+	public void setStadiums(List<Stadium> stadiums) {
+		this.stadiums = stadiums;
+	}
+
+	public DataModel<Stadium> getDatamodel() {
+		datamodel.setWrappedData(stadiumServicesLocal.findAllStadiums());
+		return datamodel;
+	}
+
+	public void setDatamodel(DataModel<Stadium> datamodel) {
+		this.datamodel = datamodel;
+	}
+
+	public UserCtr getUserCtr() {
+		return userCtr;
+	}
+
+	public void setUserCtr(UserCtr userCtr) {
+		this.userCtr = userCtr;
+	}
+
+}
